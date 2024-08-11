65ce86eb9246d151a1fc01e7296b0ec545911a8e
Yash Vardhan Singh <singhyashvardhan34@gmail.com>
2024-08-11 11:05:49 +0530

    commit message

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [{
+      name: 'StitchStyle',
+      script: 'app.js', // the path of the script you want to execute,
+      // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      error_file: 'err.log',
+      out_file: 'out.log',
+      log_file: 'combined.log',
+      time: true,
+      env: {
+      },
+    }],
+  };
