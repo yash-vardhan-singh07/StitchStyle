7dccd8008a053d887333620a2bd23f62870d385d
yash-vardhan-singh07 <150700501+yash-vardhan-singh07@users.noreply.github.com>
2025-06-26 16:25:59 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -37,6 +37,15 @@ app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
+
+
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
 const productModel=require('../server/models/productModel');
 
 const userRouter=require('./routers/userRouter'); 
